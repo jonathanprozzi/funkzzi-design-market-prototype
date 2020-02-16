d4a9e0990c0bb5c9c24713f43390d0aa535ac141
jonathanprozzi <jonathanprozzi@gmail.com>
2020-02-15 20:24:40 -0500

    added gatsby-background-image

--- src/components/aboutsection.js
+++ src/components/aboutsection.js
@@ -1,18 +1,20 @@
 import React from "react"
 import { css } from "@emotion/core"
+import BioCard from "../components/biocard"
 
 const AboutSection = () => (
   <section
     css={css`
-      position: relative;
       margin: 0 auto;
-      height: 100vh;
+      max-width: 900px;
+      min-height: 100vh;
       background: #fafafa;
     `}
   >
     <div
       css={css`
         display: flex;
+        flex-direction: column;
         align-items: center;
         justify-content: center;
       `}
@@ -25,6 +27,28 @@ const AboutSection = () => (
       >
         About Us
       </h2>
+      <div
+        css={css`
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-column-gap: 2rem;
+          grid-row-gap: 2rem;
+          margin-top: 2rem;
+
+          @media only screen and (max-width: 1000px) {
+            grid-template-columns: 1fr;
+          }
+        `}
+      >
+        <BioCard
+          name="Tracy Funk"
+          bio="By day, I am a Human Resources Specialist. However, my true passion is crafting! I'm always searching for inspiration and love discovering new mediums and developing new skills."
+        ></BioCard>
+        <BioCard
+          name="Jonathan Prozzi"
+          bio="I'm an educator, technologist, musician, and design enthuasist. I'm passionate about discovering new mediums for expression, particularly at the intersection of art and technology."
+        ></BioCard>
+      </div>
     </div>
   </section>
 )
--- src/components/biocard.js
+++ src/components/biocard.js
@@ -1,6 +1,30 @@
 import React from "react"
 import { css } from "@emotion/core"
 
-const BioCard = ({ image, name, bio }) => <div></div>
+const BioCard = ({ image, name, bio }) => (
+  <div
+    css={css`
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      border-radius: 2px 2px 0 0;
+      box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.14),
+        0 1px 18px 0 rgba(0, 0, 0, 0.12), 0 3px 5px -1px rgba(0, 0, 0, 0.3);
+      background-image: url("../images/jonathan-profile.jpg");
+      background: #ffffff;
+    `}
+  >
+    <div
+      css={css`
+        height: 100px;
+        background-color: pink;
+      `}
+    ></div>
+    <div>
+      <h3>{name}</h3>
+      <p>{bio}</p>
+    </div>
+  </div>
+)
 
 export default BioCard
