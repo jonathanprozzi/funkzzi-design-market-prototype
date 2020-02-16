6463ee79bd610aada94aa6548f6da43079b9954b
jonathanprozzi <jonathanprozzi@gmail.com>
2020-02-15 21:13:11 -0500

    changed Masthead to use new YarnSection with {children}

--- src/components/masthead.js
+++ src/components/masthead.js
@@ -1,23 +1,11 @@
 import React from "react"
-import yarnBackground from "../images/yarn-bg-1.jpg"
+import YarnSection from "../components/yarnsection"
 import { css } from "@emotion/core"
 
 const Masthead = () => {
   return (
-    <section
-      css={css`
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background-image: url(${yarnBackground});
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-position: center center;
-        min-height: 50vh;
-        box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
-          0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);
-      `}
-    >
+    <YarnSection>
+      >
       <div
         css={css`
           text-align: center;
@@ -48,7 +36,7 @@ const Masthead = () => {
           in Baltimore
         </p>
       </div>
-    </section>
+    </YarnSection>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,15 @@ import Layout from "../components/layout"
 import Header from "../components/header"
 import Masthead from "../components/masthead"
 import AboutSection from "../components/aboutsection"
+import YarnSection from "../components/yarnsection"
 
 export default () => (
   <Layout>
     <Header></Header>
     <Masthead></Masthead>
     <AboutSection></AboutSection>
+    <YarnSection>
+      <h2>Testing</h2>
+    </YarnSection>
   </Layout>
 )
