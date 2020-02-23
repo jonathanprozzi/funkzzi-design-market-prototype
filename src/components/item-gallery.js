1c8e5fe1496036554d4410b65cb5f5c8597296b8
jonathanprozzi <jonathanprozzi@gmail.com>
2020-02-23 00:42:08 -0500

    added links to all portfolio items

--- src/components/item-gallery.js
+++ src/components/item-gallery.js
@@ -1,10 +1,14 @@
 import React from "react"
 import { useStaticQuery, graphql, Link } from "gatsby"
 import { css } from "@emotion/core"
+import styled from "@emotion/styled"
 import Item from "./item"
 
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`
 // query for images in this component -> passed to each ItemCard
-
 const ItemGallery = () => {
   const { critterImage, jewelryImage } = useStaticQuery(
     graphql`
@@ -42,37 +46,49 @@ const ItemGallery = () => {
         }
       `}
     >
-      <Item
-        image={critterImage.childImageSharp.fluid}
-        alt="Crotcheted octobus"
-        title="Mystery Critters"
-        description="Crocheted critters inside a mysterious egg!"
-      ></Item>
-      <Item
-        image={jewelryImage.childImageSharp.fluid}
-        title="Jewelry"
-        description="Various textile and metal jewelry!"
-      ></Item>
-      <Item
-        image={critterImage.childImageSharp.fluid}
-        title="Plushies"
-        description="Plushies and lovies!"
-      ></Item>
-      <Item
-        image={jewelryImage.childImageSharp.fluid}
-        title="Decor"
-        description="Flowers, dreamcatchers, coasters, and other items!"
-      ></Item>
-      <Item
-        image={critterImage.childImageSharp.fluid}
-        title="Apparel"
-        description="Hats, gloves, scrunchies, and more!"
-      ></Item>
-      <Item
-        image={jewelryImage.childImageSharp.fluid}
-        title="Custom Designs"
-        description="Custom items made to order by request!"
-      ></Item>
+      <StyledLink to="/critters">
+        <Item
+          image={critterImage.childImageSharp.fluid}
+          alt="Crotcheted octobus"
+          title="Mystery Critters"
+          description="Crocheted critters inside a mysterious egg!"
+        ></Item>
+      </StyledLink>
+      <StyledLink to="/jewelry">
+        <Item
+          image={jewelryImage.childImageSharp.fluid}
+          title="Jewelry"
+          description="Various textile and metal jewelry!"
+        ></Item>
+      </StyledLink>
+      <StyledLink to="/plushies">
+        <Item
+          image={critterImage.childImageSharp.fluid}
+          title="Plushies"
+          description="Plushies and lovies!"
+        ></Item>
+      </StyledLink>
+      <StyledLink to="/decor">
+        <Item
+          image={jewelryImage.childImageSharp.fluid}
+          title="Decor"
+          description="Flowers, dreamcatchers, coasters, and other items!"
+        ></Item>
+      </StyledLink>
+      <StyledLink to="/apparel">
+        <Item
+          image={critterImage.childImageSharp.fluid}
+          title="Apparel"
+          description="Hats, gloves, scrunchies, and more!"
+        ></Item>
+      </StyledLink>
+      <StyledLink to="/custom-designs">
+        <Item
+          image={jewelryImage.childImageSharp.fluid}
+          title="Custom Designs"
+          description="Custom items made to order by request!"
+        ></Item>
+      </StyledLink>
     </div>
   )
 }
