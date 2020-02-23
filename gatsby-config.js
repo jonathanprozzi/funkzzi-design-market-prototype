eb08c27ea95dd73f0dba5ad50daa14ca8f6fb545
jonathanprozzi <jonathanprozzi@gmail.com>
2020-02-23 00:52:10 -0500

    removed typography.js and dependencies -- moved typography to global css

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,6 @@
 module.exports = {
   plugins: [
     {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
